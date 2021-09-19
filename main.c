9c98557cbc223dbcbc1ed481410a3f88041d2996
SAHIL NAZIR <74105412+SAHIL-NAZIR@users.noreply.github.com>
2021-09-19 23:04:14 +0530

    final update

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
 
 int main()
 {
-    double minutes, days, years;
-    return 0;
+    int minutes = 0;
+    double days, years;
+    printf("Enter number of minutes: ");
+    scanf("%d", &minutes);
+    days = (minutes / (60.0 * 24.0));
+    years = (minutes / (60.0 * 24.0 * 360.0));
+    printf("%d minutes are equal to %f days, and\n", minutes, days);
+    printf("%d minutes are also equal to %f years", minutes, years);
 }
